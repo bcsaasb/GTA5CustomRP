438889e59b75d9c4de2d6e657f919e3e52fe819a
Alex Johann Jacques Souza <0darkgamers0@gmail.com>
2019-04-10 16:41:40 -0300

    Update jeu_essai.sql

--- resources/[jobs]/jobs-paycheck/jeu_essai.sql
+++ resources/[jobs]/jobs-paycheck/jeu_essai.sql
@@ -4,22 +4,11 @@
 
 INSERT INTO `jobs` (`job_id`, `job_name`, `salary`)
 VALUES (1, 'Desempregado',100),
-(2, 'Policial-Rec', 1000),
-(3, 'Policial-Soldado', 1200),
-(4, 'Policial-Cabo', 1400),
-(5, 'Policial-Sargento', 1800),
-(6, 'Policial-SubTenente', 2200),
-(7, 'Policial-3Tentente', 2600),
-(8, 'Policial-2Tentente', 4000),
-(9, 'Policial-1Tentente', 4500),
-(10, 'Policial-Capitao', 5300),
-(11, 'Policial-Major', 6200),
-(12, 'Policial-Coronel', 7000),
-(13, 'Policial-Comandante', 9000),
-(14, 'Socorrista', 1200),
-(15, 'Minerador-Ouro', 1100),
-(15, 'Minerador-Diamante', 1500),
-(16, 'Mecanico', 1000);
+(2, 'Policial', 1000),
+(3, 'Socorrista', 1200),
+(4, 'Minerador-Ouro', 1100),
+(5, 'Minerador-Diamante', 1500),
+(6, 'Mecanico', 1000);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
